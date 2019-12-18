2e31d8d39c6f4c99777bf507ccfe64cc28448b13
GabrielBerberianDaitan <gberberian@daitan.com>
2019-12-18 19:20:15 -0300

    SYM-127 - Enriched Symphony events

---
+++ src/main/java/com/symphony/ms/bot/sdk/internal/event/model/StreamDetails.java
@@ -0,0 +1,40 @@
+package com.symphony.ms.bot.sdk.internal.event.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import model.Stream;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Symphony stream details
+ *
+ * @author Gabriel Berberian
+ */
+@Data
+@NoArgsConstructor
+public class StreamDetails {
+
+  private StreamType streamType;
+  private String roomName;
+  private List<UserDetails> members;
+  private Boolean external;
+  private Boolean crossPod;
+
+  public StreamDetails(Stream stream) {
+    this.streamType = StreamType.valueOf(stream.getStreamType().toUpperCase());
+    this.roomName = stream.getRoomName();
+    this.members = stream.getMembers() == null ? Collections.emptyList()
+        : stream.getMembers().stream().map(UserDetails::new).collect(Collectors.toList());
+    this.external = stream.getExternal();
+    this.crossPod = stream.getCrossPod();
+
+  }
+
+  public enum StreamType {
+    ROOM, IM, MIM
+  }
+
+}
