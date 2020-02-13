4412d47b0ede5adac9043fa15cac9a7dfeabfdfa
GabrielBerberianDaitan <gberberian@daitan.com>
2020-02-13 19:38:34 -0300

    SYM-219 - Spreadsheet presence service

--- src/main/java/com/symphony/ms/bot/sdk/internal/sse/SseController.java
+++ src/main/java/com/symphony/ms/bot/sdk/internal/sse/SseController.java
@@ -23,7 +23,6 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
@@ -84,7 +83,6 @@ public class SseController {
     }
 
     SseEmitter emitter = new SseEmitter();
-    metadata.put("subscriberUuid", UUID.randomUUID().toString());
     SseSubscriber subscriber = new SseSubscriber(emitter, eventTypes,
         metadata, lastEventId, new Long(userId), queueCapacity);
 
--- src/main/java/com/symphony/ms/bot/sdk/sse/SpreadsheetPublisher.java
+++ src/main/java/com/symphony/ms/bot/sdk/sse/SpreadsheetPublisher.java
@@ -1,6 +1,5 @@
 package com.symphony.ms.bot.sdk.sse;
 
-import com.symphony.ms.bot.sdk.internal.feature.FeatureManager;
 import com.symphony.ms.bot.sdk.internal.sse.SsePublisher;
 import com.symphony.ms.bot.sdk.internal.sse.SseSubscriber;
 import com.symphony.ms.bot.sdk.internal.sse.model.SseEvent;
@@ -8,14 +7,13 @@ import com.symphony.ms.bot.sdk.internal.sse.model.SubscriptionEvent;
 import com.symphony.ms.bot.sdk.internal.symphony.UsersClient;
 import com.symphony.ms.bot.sdk.internal.symphony.exception.SymphonyClientException;
 import com.symphony.ms.bot.sdk.internal.symphony.model.SymphonyUser;
-import com.symphony.ms.bot.sdk.spreadsheet.service.SpreadsheetPresenceEmitter;
+import com.symphony.ms.bot.sdk.spreadsheet.service.SpreadsheetPresenceService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -30,18 +28,15 @@ public class SpreadsheetPublisher extends SsePublisher {
   private static final Logger LOGGER = LoggerFactory.getLogger(SpreadsheetPublisher.class);
   private static final String SPREADSHEET_PRESENCE_EVENT = "spreadsheetPresenceEvent";
   private static final long WAIT_INTERVAL = 1000L;
-  private static final long PRESENCE_TIME_INTERVAL = 10000L;
 
   private final UsersClient usersClient;
-  private final FeatureManager featureManager;
   private final AtomicLong eventId;
-  private final Map<String, SpreadsheetPresenceEmitter> presenceEmitters;
+  private final SpreadsheetPresenceService presenceService;
 
-  public SpreadsheetPublisher(UsersClient usersClient, FeatureManager featureManager) {
+  public SpreadsheetPublisher(UsersClient usersClient, SpreadsheetPresenceService presenceService) {
     this.usersClient = usersClient;
-    this.featureManager = featureManager;
+    this.presenceService = presenceService;
     this.eventId = new AtomicLong(0);
-    this.presenceEmitters = new HashMap<>();
   }
 
   @Override
@@ -63,29 +58,36 @@ public class SpreadsheetPublisher extends SsePublisher {
 
   @Override
   protected void onSubscriberAdded(SubscriptionEvent subscriberAddedEvent) {
-    SpreadsheetPresenceEmitter presenceEmitter =
-        buildPresenceEmitter(subscriberAddedEvent, eventId);
-    presenceEmitters.put(subscriberAddedEvent.getMetadata().get("subscriberUuid"), presenceEmitter);
-    presenceEmitter.start();
+    String streamId = subscriberAddedEvent.getMetadata().get("streamId");
+    Long userId = subscriberAddedEvent.getUserId();
+
+    SymphonyUser user = getUserById(userId);
+
+    SseEvent presenceEvent = buildPresenceEvent(streamId, user);
+
+    presenceService.beginSending(eventId, presenceEvent, this, streamId, userId);
   }
 
   @Override
   protected void onSubscriberRemoved(SubscriptionEvent subscriberRemovedEvent) {
-    String subscriberUuid = subscriberRemovedEvent.getMetadata().get("subscriberUuid");
-    presenceEmitters.get(subscriberUuid).finish();
-    presenceEmitters.remove(subscriberUuid);
+    String streamId = subscriberRemovedEvent.getMetadata().get("streamId");
+    Long userId = subscriberRemovedEvent.getUserId();
+
+    presenceService.finishSending(streamId, userId);
   }
 
   public Long getIdAndIncrement() {
     return eventId.getAndIncrement();
   }
 
-  public SpreadsheetPresenceEmitter buildPresenceEmitter(
-      SubscriptionEvent subscriberAddedEvent, AtomicLong eventId) {
-    SymphonyUser user = getUserById(subscriberAddedEvent.getUserId());
-    SseEvent presenceEvent =
-        buildPresenceEvent(subscriberAddedEvent.getMetadata().get("streamId"), user);
-    return new SpreadsheetPresenceEmitter(PRESENCE_TIME_INTERVAL, presenceEvent, eventId, this);
+  private SymphonyUser getUserById(long userId) {
+    try {
+      SymphonyUser user = usersClient.getUserFromId(userId, true);
+      return user != null ? user : usersClient.getUserFromId(userId, false);
+    } catch (SymphonyClientException e) {
+      LOGGER.error("Exception getting user by id {}", userId);
+      return null;
+    }
   }
 
   private SseEvent buildPresenceEvent(String streamId, SymphonyUser user) {
@@ -103,16 +105,6 @@ public class SpreadsheetPublisher extends SsePublisher {
             put("streamId", streamId);
           }
         }).build();
-  }
-
-  private SymphonyUser getUserById(long userId) {
-    try {
-      SymphonyUser user = usersClient.getUserFromId(userId, true);
-      return user != null ? user : usersClient.getUserFromId(userId, false);
-    } catch (SymphonyClientException e) {
-      LOGGER.error("Exception getting user by id {}", userId);
-      return null;
-    }
   }
 
 }
